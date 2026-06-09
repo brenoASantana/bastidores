9e1e6d436fc7608440e6004dbcbdb620676cf9df
brenoasantana <contatobrenosantana@outlook.com>
2026-06-09 19:07:51 -0300

    feat: new version of the map

--- src/config/Constants.ts
+++ src/config/Constants.ts
@@ -9,7 +9,7 @@ export const CREATOR = 'Breno Santana' as const;
 export const WORLD = {
   GRID_BLOCK_SIZE: 5,  // Ao quadrado. Ex: 9x9
   STRUCTURE_WALL_HEIGHT: 4.5,   // Altura do pé-direito
-  ENVIRONMENT_LIGHT_INTENSITY: 0.5,
+  ENVIRONMENT_LIGHT_INTENSITY: 10,
 } as const;
 
 // 3. GAMEPLAY E MECÂNICAS
